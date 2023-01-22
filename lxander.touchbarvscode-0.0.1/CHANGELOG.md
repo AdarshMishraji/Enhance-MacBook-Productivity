6e3e1d8684e3bda2f8f5c4457f9c7d4a413eeeea
Adarsh Mishra <39414583+AdarshMishraji@users.noreply.github.com>
2023-01-23 02:48:02 +0530

    touch bar git added, and icon fix

---
+++ lxander.touchbarvscode-0.0.1/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [1.0.0]
+
+- add "⊙ Target" button (workbench.files.action.showActiveFileInExplorer)
+- fix images of description
+
+## [0.3.0]
+
+- add Day/Night button
+
+## [0.2.1]
+
+- fix readme
+
+## [0.2.0]
+
+- Add "Active bar" groupe
+- Add table documentation
+
+## [0.1.1]
+
+- Add more buttons
+- Fix default values
+
+## [0.1.0]
+
+- Add core buttons
+
+## [0.0.2]
+
+- Add documentation
+
+## [0.0.1]
+
+- Initial release
--- lxander.touchbarvscode-0.0.1/out/extension.js
+++ lxander.touchbarvscode-0.0.1/out/extension.js
@@ -11,22 +11,61 @@ const getSettings = () => {
         day: extensionConfig.dayTheme
     };
 };
+
+const showInfo = (infoMsg) =>
+    vscode.window.showInformationMessage(infoMsg);
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
     let disposable = vscode.workspace.onDidChangeConfiguration(getSettings, this);
     let themeKey = 'workbench.colorTheme';
     let settings = getSettings();
-    vscode.commands.registerCommand('extensionTouchBar.night', () => {
+    vscode.commands.registerCommand('lxander.touchbar.night', () => {
         userConfig.update(themeKey, settings.night, true);
     });
-    vscode.commands.registerCommand('extensionTouchBar.day', () => {
+    vscode.commands.registerCommand('lxander.touchbar.day', () => {
         userConfig.update(themeKey, settings.day, true);
     });
-    context.subscriptions.push(disposable);
+
+    const syncBranch = vscode.commands.registerCommand("lxander.touchbar.syncBranch", () => {
+        vscode.commands.executeCommand("git.sync");
+        showInfo("Branch synced");
+    });
+
+    const commit = vscode.commands.registerCommand("lxander.touchbar.gitcommit", () => {
+        vscode.commands.executeCommand("git.commit");
+        showInfo("Changes commited successfully");
+    });
+
+    const push = vscode.commands.registerCommand("lxander.touchbar.gitpush", () => {
+        vscode.commands.executeCommand("git.push");
+        showInfo("Changes pushed successfully");
+    });
+
+    const pull = vscode.commands.registerCommand("lxander.touchbar.gitpull", () => {
+        vscode.commands.executeCommand("git.pull");
+        showInfo("Changes pulled successfully");
+    });
+
+    const checkout = vscode.commands.registerCommand("lxander.touchbar.checkoutTo", () => {
+        vscode.commands.executeCommand("git.checkout");
+    });
+
+    const stageAll = vscode.commands.registerCommand("lxander.touchbar.stageAll", () => {
+        vscode.commands.executeCommand("git.stageAll");
+        showInfo("Changes Staged");
+    });
+
+    const toggleFocusMode = vscode.commands.registerCommand('lxander.touchbar.enableFocusMode', () => {
+        vscode.commands.executeCommand('workbench.action.closePanel');
+        vscode.commands.executeCommand('workbench.action.closeSidebar');
+        vscode.commands.executeCommand('workbench.action.closeAuxiliaryBar');
+    });
+    context.subscriptions.push(disposable, toggleFocusMode, syncBranch, commit, push, pull, closeGit, checkout, stageAll);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
-//# sourceMappingURL=extension.js.map
+//# sourceMappingURL=lxander.touchbar.js.map
